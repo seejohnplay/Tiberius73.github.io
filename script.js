5afab6ad9c0add40030c984db964a8d6a67ba2f1
Tiberius <ciufleat@yahoo.com>
2020-11-24 13:00:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myMiddleName() {
+  var mid = document.getElementById("myDIV");
+  if (mid.innerHTML === "01000110") {
+    mid.innerHTML = "F";
+  } else {
+    mid.innerHTML = "01000110";
+  }
+}
