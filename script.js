6e893b25817dff2a68376f48b5f79a3a0134b596
Tiberius <ciufleat@yahoo.com>
2021-06-28 12:46:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
  var mid = document.getElementById("myDIV");
 function myMiddleName() {
-  
-  if (mid.innerHTML = "01000110") {
+   if (mid.innerHTML = "01000110") {
     mid.innerHTML = "F";
   } else {
     mid.innerHTML = "01000110";
